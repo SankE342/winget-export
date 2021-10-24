cd85d7fb7576a50e5b3cc6975a6984daf9ceff7e
sanke342 <gesm342@gmail.com>
2021-10-24 12:34:56 -0500

    Python scrip modified to make it easier to read. No changes in the funcionality of the application expected.

--- batch_builder.py
+++ batch_builder.py
@@ -1,43 +1,45 @@
 # %%
 
-from os     import system as cmd
+from os     import system
 from json   import loads
 
 # %%
-
-Dir = input('''
+path = input('''
 Enter directory to save files to (Example: D:/Folder1/Folder2/)
 To use current directory, leave blank:
 '''
 )
-if (Dir == ''): Dir = './'
-elif (Dir[-1] != '/'): Dir += '/'
-File = 'winget_packages.json'   # JSON export of all packages
-Exec = 'package_installer.bat'  # Batch output
+if (path == ''): path = './'
+elif (path[-1] != '/'): path += '/'
+file_name = 'winget_packages.json'   # JSON export of all packages
+exec_name = 'package_installer.bat'  # Batch output
 
 # %%
 
 print('Exporting list of installed programs as a JSON file')
-cmd(f'winget export \"{Dir + File}\"')
+system(f'winget export \"{path + file_name}\"')
 print()
 
 # %%
 
 print('Loading JSON file to Python and creating a list of packages')
-with open(Dir + File, 'r') as hdlFile:
-    dctFile = loads(hdlFile.read())
+with open(path + file_name, 'r') as file_hdl:
+    file_dct = loads(file_hdl.read())
 
-lstPackages = [ package['PackageIdentifier'] for package in dctFile['Sources'][0]['Packages'] ]
+packages_lst = [
+    package['PackageIdentifier']
+    for package in file_dct['Sources'][0]['Packages']
+]
 print()
 
 # %%
 
 # Building the Batch script
 print('Exported programs available through winget')
-with open(Dir + Exec, 'w') as hdlExec:
-    for package in lstPackages:
-        hdlExec.write(f'winget install -s winget -e --id --force {package}\n')
+with open(path + exec_name, 'w') as exec_hdl:
+    for package in packages_lst:
+        exec_hdl.write(f'winget install -s winget -e --id --force {package}\n')
         print(f'Package id: {package}')
 print()
 
-input(f'Task completed succesfully. To install the packages in the new PC/OS, transfer and run "{Exec}"\nPress ENTER to finish')
+input(f'Task completed succesfully. To install the packages in the new PC/OS, transfer and run "{exec_name}"\nPress ENTER to finish')
